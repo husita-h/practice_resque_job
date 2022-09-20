4c7c9af4b2bfa9d5eaabeb65a4a3be1abc5bad35
husita-h <taisei.fukushi1129@gmail.com>
2022-09-21 00:43:11 +0900

    ./config/routes.rbにResque 用の定義を追加する

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
+require 'resque/server'
+
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'users/:name', :to => 'users#show'
+  mount Resque::Server.new, at: "/resque"
 end
